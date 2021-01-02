cd557e9e1017abb942a0fc1fefbe715c43d72fda
sizzlorox <sizzlorox@protonmail.com>
2021-01-02 17:39:24 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+import Header from './containers/Header';
+import LaunchInfo from './containers/LaunchInfo';
+
 import './App.css';
+import './index.css';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Header />
+        <LaunchInfo />
+      </div>
+    </QueryClientProvider>
   );
 }
-
 export default App;
---
+++ src/containers/LaunchInfo.jsx
@@ -0,0 +1,53 @@
+import { useQuery } from 'react-query';
+import { utcToZonedTime, format } from 'date-fns-tz';
+
+const getHeaderImage = (links) => links.length
+  ? (
+    <img class="object-cover object-center w-full h-40 mb-6 rounded" src={links[0]} alt="content" />
+  )
+  : null;
+
+const formatTimeZone = (utcDate) => {
+  const zonedDate = utcToZonedTime(new Date(utcDate), Intl.DateTimeFormat().resolvedOptions().timeZone);
+  return format(zonedDate, 'yyyy-MM-dd HH:mm:ss [OOOO]');
+};
+
+function LaunchInfo() {
+  const { isLoading, error, data } = useQuery('launch', () => fetch('https://api.spacexdata.com/v4/launches/upcoming').then(res => res.json()));
+
+  if (isLoading) return '...';
+
+  if (error) return 'Oh shit ' + error;
+
+  console.log(data[0])
+
+  return (
+    <section class="text-gray-700 body-font">
+      <div class="container px-8 mx-auto py-36 lg:px-4">
+        <div class="flex flex-wrap text-left">
+          {
+            data.map(launchData => (
+              <div class="px-8 py-6 lg:w-1/3 md:w-full">
+                {getHeaderImage(launchData.links.flickr.original)}
+                <h2 class="mb-3 text-lg font-semibold text-gray-700 lg:text-2xl title-font">
+                  {launchData.name} - {formatTimeZone(launchData.date_utc)}
+                </h2>
+                <p class="mb-4 text-base leading-relaxed">
+                  {launchData.details}
+                </p>
+                <a href="#" class="inline-flex items-center font-semibold text-blue-700 md:mb-2 lg:mb-0 hover:text-blue-400 ">
+                  Read More
+                  <svg class="w-4 h-4 ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
+                    <path fill="none" d="M0 0h24v24H0z" />
+                    <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" />
+                  </svg>
+                </a>
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    </section>
+  );
+};
+export default LaunchInfo;
