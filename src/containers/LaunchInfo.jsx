a2fe434f28688954b42ac373801af8679d1452c2
sizzlorox <sizzlorox@protonmail.com>
2021-01-02 19:19:11 +0800

    fix(launchinfo): different sorting for upcoming and past launches

--- src/containers/LaunchInfo.jsx
+++ src/containers/LaunchInfo.jsx
@@ -1,5 +1,5 @@
 import { useQuery } from 'react-query';
-import { compareDesc } from 'date-fns';
+import { compareDesc, compareAsc } from 'date-fns';
 import { utcToZonedTime, format } from 'date-fns-tz';
 import { Link, withRouter } from 'react-router-dom';
 
@@ -28,24 +28,27 @@ function LaunchInfo({
       <div class="container px-8 mx-auto py-36 lg:px-4">
         <div class="flex flex-wrap text-left">
           {
-            data && data.sort((a, b) => compareDesc(new Date(a.date_utc), new Date(b.date_utc))).map(launchData => (
-              <div class="px-8 py-6 lg:w-1/3 md:w-full">
-                {getHeaderImage(launchData.links.flickr.original)}
-                <h2 class="mb-3 text-lg font-semibold text-gray-700 lg:text-2xl title-font">
-                  {launchData.name} - {formatTimeZone(launchData.date_utc)}
-                </h2>
-                <p class="mb-4 text-base leading-relaxed">
-                  {launchData.details}
-                </p>
-                <Link class="inline-flex items-center font-semibold text-blue-700 md:mb-2 lg:mb-0 hover:text-blue-400" to={`/launch/${launchData.id}`}>
-                  Read More
-                  <svg class="w-4 h-4 ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
-                    <path fill="none" d="M0 0h24v24H0z" />
-                    <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" />
-                  </svg>
-                </Link>
-              </div>
-            ))
+            data && data.sort((a, b) => launchType === 'upcoming'
+              ? compareAsc(new Date(a.date_utc), new Date(b.date_utc))
+              : compareDesc(new Date(a.date_utc), new Date(b.date_utc)))
+              .map(launchData => (
+                <div class="px-8 py-6 lg:w-1/3 md:w-full">
+                  {getHeaderImage(launchData.links.flickr.original)}
+                  <h2 class="mb-3 text-lg font-semibold text-gray-700 lg:text-2xl title-font">
+                    {launchData.name} - {formatTimeZone(launchData.date_utc)}
+                  </h2>
+                  <p class="mb-4 text-base leading-relaxed">
+                    {launchData.details}
+                  </p>
+                  <Link class="inline-flex items-center font-semibold text-blue-700 md:mb-2 lg:mb-0 hover:text-blue-400" to={`/launch/${launchData.id}`}>
+                    Read More
+                    <svg class="w-4 h-4 ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="currentColor">
+                      <path fill="none" d="M0 0h24v24H0z" />
+                      <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" />
+                    </svg>
+                  </Link>
+                </div>
+              ))
           }
         </div>
       </div>
